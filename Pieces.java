54acc9a6f455b09465693400db03dc392127d389
ngfriedman <nina.friedman@coloradocollege.edu>
2014-09-19 15:12:24 -0600

    Create Pieces.java

---
+++ Pieces.java
@@ -0,0 +1,21 @@
+// JFC
+import java.awt.*;
+import java.awt.event.*;
+
+// GTGE
+import com.golden.gamedev.*;
+import com.golden.gamedev.object.*;
+
+
+public class Pieces{
+
+	public piece1(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
+	    super(image, xLocation, yLocation);
+	    
+	}
+
+	public piece2(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
+	    super(image, xLocation, yLocation);
+	    
+	}
+    }
