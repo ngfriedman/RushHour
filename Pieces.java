1763d93c95dcda604ebfb976c5e3b69f41a19cfa
ngfriedman <nina.friedman@coloradocollege.edu>
2014-09-19 15:17:32 -0600

    Update Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -7,15 +7,20 @@ import com.golden.gamedev.*;
 import com.golden.gamedev.object.*;
 
 
-public class Pieces{
+public class Pieces {
+}
 
-	public piece1(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	    super(image, xLocation, yLocation);
+class piece1 {
+    public piece1(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
+	super(image, xLocation, yLocation);
 	    
-	}
-
-	public piece2(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	    super(image, xLocation, yLocation);
-	    
-	}
     }
+}
+
+class piece2 {
+    public piece2(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
+	super(image, xLocation, yLocation);
+	    
+    }
+}
+
