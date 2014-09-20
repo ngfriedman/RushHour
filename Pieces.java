5cd666952f20c1bc21215a92b67301a4ba5bb402
ngfriedman <nina.friedman@coloradocollege.edu>
2014-09-20 17:14:56 -0600

    Update Pieces.java

--- Pieces.java
+++ Pieces.java
@@ -6,37 +6,55 @@ import com.golden.gamedev.*;
 import com.golden.gamedev.object.*;
 
 
-public class Pieces {
+public class Pieces extends Sprite{
+
+    double movementSpeed = 0;
+
+    public Pieces(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
+	super(image, xLocation, yLocation);
+    }
+
+    public void setMovementSpeed(double newSpeed) {
+    	movementSpeed = newSpeed;
+    }
+	    
 }
 
-class piece1 extends Sprite {
-    public piece1(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	super(image, xLocation, yLocation);
+class Piece1 extends Pieces {
+
+    public Piece1(java.awt.image.BufferedImage _image, int _xLocation, int _yLocation) {
+	super(_image, _xLocation, _yLocation);
 	    
     }
 }
 
-class piece2 extends Sprite {
-    public piece2(java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	super(image, xLocation, yLocation);
+class Piece2 extends Pieces{
+
+    public Piece2(java.awt.image.BufferedImage _image, int _xLocation, int _yLocation) {
+	super(_image, _xLocation, _yLocation);
 	    
     }
 }
-class piece3 extends Sprite{
-    public piece3 (java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	super( image, xLocation, yLocation);
+class Piece3 extends Pieces{
+
+    double movementSpeed = 0;
+
+    public Piece3 (java.awt.image.BufferedImage _image, int _xLocation, int _yLocation) {
+	super( _image, _xLocation, _yLocation);
     }
 }
 
-class piece4 extends Sprite{
-    public piece4 (java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	super (image, xLocation, yLocation);
+class Piece4 extends Pieces{
+
+    public Piece4 (java.awt.image.BufferedImage _image, int _xLocation, int _yLocation) {
+	super (_image, _xLocation, _yLocation);
     }
 }
 
-class piece5 extends Sprite {
-    public piece5 (java.awt.image.BufferedImage image, int xLocation, int yLocation) {
-	super (image, xLocation, yLocation);
+class Piece5 extends Pieces {
+
+    public Piece5 (java.awt.image.BufferedImage _image, int _xLocation, int _yLocation) {
+	super (_image, _xLocation, _yLocation);
     }
 }
 
